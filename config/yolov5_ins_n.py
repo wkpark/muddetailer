063e131a48d8b262a03180d120d6a64e04ed329d
Won-Kyu Park <wkpark@gmail.com>
2023-10-09 17:21:40 +0900

    mmdet/mmyolo configs added

---
+++ config/yolov5_ins_n.py
@@ -0,0 +1,10 @@
+_base_ = 'yolov5_ins_s-v61_syncbn_fast_8xb16-300e_coco_instance.py'
+
+deepen_factor = 0.33
+widen_factor = 0.25
+
+model = dict(
+    data_preprocessor=dict(bgr_to_rgb=False),
+    backbone=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    neck=dict(deepen_factor=deepen_factor, widen_factor=widen_factor),
+    bbox_head=dict(head_module=dict(widen_factor=widen_factor)))
